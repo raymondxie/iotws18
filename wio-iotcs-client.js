1cf1064970021eb96dfd0f5c189bc8c14f45327c
yuhua_xie <yuhua.xie@oracle.com>
2017-11-29 11:14:36 -0800

    add logic outline for Wio based IoTCS client

---
+++ wio-iotcs-client.js
@@ -0,0 +1,5 @@
+// working on it
+//
+// 1. create async polling of sensor data from Wio-* boards
+// 2. Map sensor data/actuactor control signal to Device Model in IoTCS
+// 3. Sync data/control signal with IoTCS
