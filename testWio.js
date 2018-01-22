8bb0ce007cc1427f6a8ec29365b6a43fdb97144a
yuhua_xie <yuhua.xie@oracle.com>
2018-01-22 12:48:53 -0600

    re-order callback

--- testWio.js
+++ testWio.js
@@ -1,6 +1,18 @@
 var WioNode = require("./wio.js");
 var wioConfig = require("./sensor-config.js").wio_iot;
 
+//
+// As example, I just use one callback function to display data
+//
+// You may want to supply an anonymous function to handle the async callback from Wio board interfaction.
+// in each of you board.write(), board.read() or board.stream() call.
+var callback = function(data, error) {
+    if( data != null )
+        console.log("data->", data);
+    if( error != null)
+        console.log("error->", error);
+}
+
 // construct a Wio board
 var board = new WioNode({
     "debug": true,
@@ -27,15 +39,4 @@ setTimeout(function(){
 }, 10000);
 
 
-//
-// As example, I just use one callback function to display data
-//
-// You may want to supply an anonymous function to handle the async callback from Wio board interfaction.
-// in each of you board.write(), board.read() or board.stream() call.
-var callback = function(data, error) {
-    if( data != null )
-        console.log("data->", data);
-    if( error != null)
-        console.log("error->", error);
-}
 
